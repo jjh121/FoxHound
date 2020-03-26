4919b28f43c570c11c977f41e92519744cd2ec15
Jonathan Hart <jjh121@gmail.com>
2020-03-26 10:02:53 -0400

    Simple extension method to add local appsettings

--- FoxHound.Web/Program.cs
+++ FoxHound.Web/Program.cs
@@ -4,11 +4,10 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.IO;
-using System.Linq;
 
 namespace FoxHound.Web
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -35,9 +34,7 @@ namespace FoxHound.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    // Ensure startup receives all configuration, including all appsettings.x.json files
-                    var configurationRoot = new ConfigurationRoot(GetCurrentConfiguration().Build().Providers.ToList());
-                    webBuilder.ConfigureAppConfiguration(x => x.AddConfiguration(configurationRoot));
+                    webBuilder.ConfigureAppConfiguration(config => config.AddLocalAppSettings());
                     webBuilder.UseStartup<Startup>();
                 })
                 .UseSerilog();
@@ -49,10 +46,15 @@ namespace FoxHound.Web
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: false, reloadOnChange: true)
-                .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
+                .AddLocalAppSettings()
                 .AddEnvironmentVariables();
 
             return configuration;
+        }
+
+        private static IConfigurationBuilder AddLocalAppSettings(this IConfigurationBuilder builder)
+        {
+            return builder.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
         }
 
         private static ILogger CreateLogger(IConfigurationRoot currentConfiguration)
