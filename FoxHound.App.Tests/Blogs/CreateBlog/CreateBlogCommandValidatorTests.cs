2148776457ae241df4e19f8825b4e4637ae22c3d
Tommy E <temricko@gmail.com>
2020-03-27 20:27:15 -0400

    CreateBlog Fluent Validation PR corrections

--- FoxHound.App.Tests/Blogs/CreateBlog/CreateBlogCommandValidatorTests.cs
+++ FoxHound.App.Tests/Blogs/CreateBlog/CreateBlogCommandValidatorTests.cs
@@ -2,9 +2,6 @@ using AutoFixture;
 using AutoFixture.AutoMoq;
 using FluentValidation.TestHelper;
 using FoxHound.App.Blogs.CreateBlog;
-using FoxHound.App.Data;
-using Microsoft.EntityFrameworkCore;
-using System;
 using Xunit;
 
 namespace FoxHound.App.Tests.Blogs.CreateBlog
@@ -12,17 +9,11 @@ namespace FoxHound.App.Tests.Blogs.CreateBlog
     public class CreateBlogCommandValidatorTests
     {
         private readonly IFixture _fixture;
-        private readonly IFoxHoundData _foxHoundData;
-        private readonly DbContextOptions<FoxHoundData> _dbContextOptions;
 
         public CreateBlogCommandValidatorTests()
         {
             _fixture = new Fixture();
             _fixture.Customize(new AutoMoqCustomization());
-
-            _dbContextOptions = new DbContextOptionsBuilder<FoxHoundData>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
-            _foxHoundData = new FoxHoundData(_dbContextOptions);
-            _fixture.Inject(_foxHoundData);
         }
 
         [Fact]
@@ -33,6 +24,16 @@ namespace FoxHound.App.Tests.Blogs.CreateBlog
 
             // Act / Assert
             validator.ShouldHaveValidationErrorFor(x => x.Owner, string.Empty).WithErrorMessage("Owner is required");
+        }
+
+        [Fact]
+        public void Owner_IsGreaterThan20Characters_FailsValidation()
+        {
+            // Arrange
+            var validator = _fixture.Create<CreateBlogCommandValidator>();
+
+            // Act / Assert
+            validator.ShouldHaveValidationErrorFor(x => x.Owner, new string('*', 21)).WithErrorMessage("Owner must be less than or equal to 20 characters");
         }
     }
 }
--- FoxHound.App/Blogs/CreateBlog/CreateBlogCommandValidator.cs
+++ FoxHound.App/Blogs/CreateBlog/CreateBlogCommandValidator.cs
@@ -7,7 +7,8 @@ namespace FoxHound.App.Blogs.CreateBlog
         public CreateBlogCommandValidator()
         {
             RuleFor(x => x.Owner)
-                .NotEmpty().WithMessage("Owner is required");
+                .NotEmpty().WithMessage("Owner is required")
+                .MaximumLength(20).WithMessage("Owner must be less than or equal to {MaxLength} characters");
         }
     }
 }
