fcee1c546a0f6d01830c985027888ca5bd0b1c27
Jon Hart <62618553+jjh121@users.noreply.github.com>
2020-03-30 17:59:36 -0400

    Add react with simple blog structure (#12)

---
+++ FoxHound.Client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Blogs from "./Blogs";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>Hello from react</div>
+      <Blogs />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
