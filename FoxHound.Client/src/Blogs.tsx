43030717f1988098aace615b2b742c466adc465b
Jonathan Hart <jjh121@gmail.com>
2020-03-31 15:55:05 -0400

    Call API from React with CORS

--- FoxHound.Client/src/Blogs.tsx
+++ FoxHound.Client/src/Blogs.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import Blog from "./Blog";
+import axios from "axios";
 
 export interface BlogModel {
   blogId: number;
@@ -7,60 +8,97 @@ export interface BlogModel {
   createdDate: Date;
 }
 
-const blogsExample: BlogModel[] = [
-  { blogId: 1, owner: "Jon", createdDate: new Date() },
-  { blogId: 2, owner: "Tim", createdDate: new Date() },
-  { blogId: 3, owner: "Ryan", createdDate: new Date() },
-  { blogId: 4, owner: "Kevin", createdDate: new Date() },
-];
-
 const Blogs: React.FC = () => {
   const [blogs, setBlogs] = useState<BlogModel[]>([]);
   const [owner, setOwner] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
-    setBlogs(blogsExample);
+    getBlogs();
   }, []);
 
-  const addBlog = () => {
-    const newBlog: BlogModel = {
-      blogId: Math.floor(Math.random() * 100),
-      owner: owner,
-      createdDate: new Date(),
-    };
+  const getBlogs = async () => {
+    try {
+      setIsLoading(true);
 
-    setBlogs((prevBlogs) => [...prevBlogs, newBlog]);
-    setOwner("");
+      const response = await axios.get<BlogModel[]>(
+        "https://localhost:44360/Blog/GetAll"
+      );
+
+      setBlogs(response.data);
+    } catch (ex) {
+      setError(ex.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const addBlog = async () => {
+    try {
+      const response = await axios.post<number>(
+        "https://localhost:44360/Blog/Create",
+        {
+          title: title,
+          owner: owner,
+        }
+      );
+
+      const blogId = response.data;
+
+      setOwner("");
+      setTitle("");
+
+      getBlogs();
+    } catch (ex) {
+      setError(ex.message);
+    }
   };
 
   return (
     <div>
       <h3>Blogs</h3>
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.blogId}>
-            <Blog blog={blog} />
-          </li>
-        ))}
-      </ul>
 
-      <input
-        type="text"
-        value={owner}
-        onChange={(event: ChangeEvent<HTMLInputElement>) =>
-          setOwner(event.target.value)
-        }
-      />
+      {error && <div>{error}</div>}
 
-      <button
-        onClick={() => {
-          addBlog();
-        }}
-      >
-        Add Blog
-      </button>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.blogId}>
+              <Blog blog={blog} />
+            </li>
+          ))}
+        </ul>
+      )}
 
-      <div>Owner: {owner}</div>
+      <div>
+        <span>Owner: </span>
+        <input
+          type="text"
+          value={owner}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setOwner(event.target.value)
+          }
+        />
+      </div>
+
+      <div>
+        <span>Title: </span>
+        <input
+          type="text"
+          value={title}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setTitle(event.target.value)
+          }
+        />
+      </div>
+
+      <div>
+        <button onClick={addBlog}>Add Blog</button>
+      </div>
     </div>
   );
 };
