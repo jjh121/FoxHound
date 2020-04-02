a1ca4f55620478e3834beaf5229eac138a1f4c02
Tommy E <temricko@gmail.com>
2020-04-02 08:03:33 -0400

    Breakout add blog to it's own component

---
+++ FoxHound.Client/src/AddBlog.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+interface IProps {
+  refreshBlogList: () => void;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2, 3),
+  },
+  templateBuilder: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+const AddBlog: React.FC<IProps> = (props) => {
+  const [owner, setOwner] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const { refreshBlogList } = props;
+
+  useEffect(() => {}, []);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      setIsSubmitting(true);
+      const response = await axios.post<number>(
+        "https://localhost:44360/Blog/Create",
+        {
+          title: title,
+          owner: owner,
+        }
+      );
+
+      const blogId = response.data;
+      setOwner("");
+      setTitle("");
+      setError("");
+      refreshBlogList();
+    } catch (ex) {
+      setError(ex.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const classes = useStyles({});
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Container maxWidth="sm">
+          <Paper className={classes.root} elevation={5}>
+            {error && <div>{error}</div>}
+            <Typography variant="h5">Add Blog</Typography>
+            <Grid container direction="column">
+              <Grid item>
+                <TextField
+                  label="Owner"
+                  value={owner}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setOwner(event.target.value)
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Title"
+                  value={title}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setTitle(event.target.value)
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <Box mt={2}>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    disabled={isSubmitting}
+                  >
+                    Add Blog
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
+      </form>
+    </div>
+  );
+};
+
+export default AddBlog;
--- FoxHound.Client/src/Blogs.tsx
+++ FoxHound.Client/src/Blogs.tsx
@@ -1,14 +1,7 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import Blog from "./Blog";
 import axios from "axios";
-import {
-  Paper,
-  Box,
-  TextField,
-  Grid,
-  Button,
-  Typography,
-} from "@material-ui/core";
+import AddBlog from "./AddBlog";
 
 export interface BlogModel {
   blogId: number;
@@ -18,14 +11,16 @@ export interface BlogModel {
 
 const Blogs: React.FC = () => {
   const [blogs, setBlogs] = useState<BlogModel[]>([]);
-  const [owner, setOwner] = useState<string>("");
-  const [title, setTitle] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
   useEffect(() => {
     getBlogs();
   }, []);
+
+  const refreshBlogList = () => {
+    getBlogs();
+  };
 
   const getBlogs = async () => {
     try {
@@ -40,27 +35,6 @@ const Blogs: React.FC = () => {
       setError(ex.message);
     } finally {
       setIsLoading(false);
-    }
-  };
-
-  const addBlog = async () => {
-    try {
-      const response = await axios.post<number>(
-        "https://localhost:44360/Blog/Create",
-        {
-          title: title,
-          owner: owner,
-        }
-      );
-
-      const blogId = response.data;
-
-      setOwner("");
-      setTitle("");
-
-      getBlogs();
-    } catch (ex) {
-      setError(ex.message);
     }
   };
 
@@ -82,39 +56,7 @@ const Blogs: React.FC = () => {
         </ul>
       )}
 
-      <Paper elevation={5}>
-        <Box p={2}>
-          <Typography variant="h5">Add Blog</Typography>
-
-          <Grid container direction="column">
-            <Grid item>
-              <TextField
-                label="Owner"
-                value={owner}
-                onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                  setOwner(event.target.value)
-                }
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                label="Title"
-                value={title}
-                onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                  setTitle(event.target.value)
-                }
-              />
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Box mt={2}>
-              <Button variant="contained" color="primary" onClick={addBlog}>
-                Add Blog
-              </Button>
-            </Box>
-          </Grid>
-        </Box>
-      </Paper>
+      <AddBlog refreshBlogList={refreshBlogList} />
     </div>
   );
 };
