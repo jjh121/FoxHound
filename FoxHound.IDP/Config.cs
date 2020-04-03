22fe1e5d7379c020cddab28a84c7949261ae7e9c
Christopher Pollaro <19241781+chrispollaro@users.noreply.github.com>
2020-04-03 09:12:46 -0400

    Setup initial Identity Server

---
+++ FoxHound.IDP/Config.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace FoxHound.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+                { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client()
+                {
+                    ClientName = "FoxHound",
+                    ClientId = "foxhound",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        //"https://localhost:44389/signout-callback-oidc"
+                    },
+                    RedirectUris = new List<string>( )
+                    {
+                       // "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OpenId
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())  // use different for production
+                    }
+                }
+
+            };
+
+    }
+}
+
---
+++ FoxHound.IDP/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FoxHound.IDP
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
