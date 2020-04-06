2d6b0776ea7f8a59c6bee8998e62f4a5ea22c851
DJ Collins <djcollins86@gmail.com>
2020-04-06 16:03:00 -0400

    Added Navigation Bar

--- FoxHound.Client/src/App.tsx
+++ FoxHound.Client/src/App.tsx
@@ -2,13 +2,22 @@ import "react-app-polyfill/stable";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import Blogs from "./Blogs";
+import NavDrawer from "./Navigation/NavDrawer";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <>
-      <div>Hello from react</div>
-      <Blogs />
+      <Router>
+        <NavDrawer />
+      </Router>
     </>
   );
 };
