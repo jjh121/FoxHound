d109f444fca262bcfeba408acbdfc4835629108d
Tommy E <temricko@gmail.com>
2020-04-09 19:45:15 -0400

    Edit Blog API/Endpoints and associated Tests

---
+++ FoxHound.App/Blogs/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using FoxHound.App.Data;
+using FoxHound.App.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoxHound.App.Blogs.UpdateBlog
+{
+    public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, Unit>
+    {
+        private readonly IFoxHoundData _foxHoundData;
+
+        public UpdateBlogCommandHandler(IFoxHoundData foxHoundData)
+        {
+            _foxHoundData = foxHoundData;
+        }
+
+        public async Task<Unit> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+        {
+            Blog blog = await _foxHoundData.Blogs
+                .SingleAsync(x => x.BlogId == request.BlogId, cancellationToken);
+
+            blog.SetOwner(request.Owner);
+            blog.SetTitle(request.Title);
+            await _foxHoundData.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ FoxHound.App/Blogs/UpdateBlog/UpdateBlogCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace FoxHound.App.Blogs.UpdateBlog
+{
+    public class UpdateBlogCommandValidator : AbstractValidator<UpdateBlogCommand>
+    {
+        public UpdateBlogCommandValidator()
+        {
+            RuleFor(x => x.BlogId)
+                .NotEmpty().WithMessage("Blog Id is required");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required")
+                .MaximumLength(128).WithMessage("Title must be less than or equal to {MaxLength} characters");
+
+            RuleFor(x => x.Owner)
+                .NotEmpty().WithMessage("Owner is required")
+                .MaximumLength(20).WithMessage("Owner must be less than or equal to {MaxLength} characters");
+        }
+    }
+}
