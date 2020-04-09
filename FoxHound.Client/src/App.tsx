b1308d2f49db97c064b330a203a49d53efb68866
Tom Navarra <tnavarra@gmail.com>
2020-04-09 12:17:28 -0400

    Add ApplicationInsights client metrics capture

--- FoxHound.Client/src/App.tsx
+++ FoxHound.Client/src/App.tsx
@@ -4,13 +4,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavDrawer from "./Navigation/NavDrawer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import {
+  ReactPlugin,
+  withAITracking,
+} from "@microsoft/applicationinsights-react-js";
+import { createBrowserHistory } from "history";
 
-const App: React.FC = () => {
+const browserHistory = createBrowserHistory({ basename: "" });
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: process.env.APP_INSIGHTS_INSTRUMENTATION_KEY,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+  },
+});
+appInsights.loadAppInsights();
+
+const App: React.ComponentClass = withAITracking(reactPlugin, () => {
   return (
     <Router>
       <NavDrawer />
     </Router>
   );
-};
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
