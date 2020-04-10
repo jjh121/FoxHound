e4e2080c2466589bcd0532eb6d4b57b89c008e64
Tom Navarra <tnavarra@gmail.com>
2020-04-10 13:47:56 -0400

    Move App Insights configuration to a separate file

--- FoxHound.Client/src/App.tsx
+++ FoxHound.Client/src/App.tsx
@@ -4,27 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavDrawer from "./Navigation/NavDrawer";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ApplicationInsights } from "@microsoft/applicationinsights-web";
-import {
-  ReactPlugin,
-  withAITracking,
-} from "@microsoft/applicationinsights-react-js";
-import { createBrowserHistory } from "history";
+import withAppInsights from "./Utility/AppInsights";
 
-const browserHistory = createBrowserHistory({ basename: "" });
-const reactPlugin = new ReactPlugin();
-const appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey: process.env.APP_INSIGHTS_INSTRUMENTATION_KEY,
-    extensions: [reactPlugin],
-    extensionConfig: {
-      [reactPlugin.identifier]: { history: browserHistory },
-    },
-  },
-});
-appInsights.loadAppInsights();
-
-const App: React.ComponentClass = withAITracking(reactPlugin, () => {
+const App: React.ComponentClass = withAppInsights(() => {
   return (
     <Router>
       <NavDrawer />
