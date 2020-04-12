50ed082f9acbea038fca6885991509a3fb707512
Christopher Pollaro <19241781+chrispollaro@users.noreply.github.com>
2020-04-12 09:39:49 -0400

    Updated Identity Server

--- FoxHound.IDP/Config.cs
+++ FoxHound.IDP/Config.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityModel;
 
 namespace FoxHound.IDP
 {
@@ -16,7 +17,36 @@ namespace FoxHound.IDP
 
         public static IEnumerable<ApiResource> Apis =>
             new ApiResource[]
-                { };
+                { 
+                    new ApiResource
+                    {
+                        Name = "foxhoundapi",
+
+                        // secret for using introspection endpoint
+                        ApiSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+
+                        // include the following using claims in access token (in addition to subject id)
+                        UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Email },
+
+                        // this API defines two scopes
+                        Scopes =
+                        {
+                            new Scope()
+                            {
+                                Name = "foxhound.full_access",
+                                DisplayName = "Full access to FoxHound API",
+                            },
+                            new Scope
+                            {
+                                Name = "foxhound.read_only",
+                                DisplayName = "Read only access to FoxHound API"
+                            }
+                        }
+                    }
+            };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
@@ -27,13 +57,39 @@ namespace FoxHound.IDP
                     ClientId = "foxhound",
                     AllowedGrantTypes = GrantTypes.Code,
                     RequirePkce = true,
-                    PostLogoutRedirectUris = new List<string>
+
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+
+                        PostLogoutRedirectUris = new List<string>
                     {
                         //"https://localhost:44389/signout-callback-oidc"
                     },
                     RedirectUris = new List<string>( )
                     {
                        // "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        "foxhound.read_only",
+                        "foxhound.full_access"
+                    },
+                    AllowedCorsOrigins = { "http://localhost:5003" }
+                },
+                new Client()
+                {
+                    ClientName = "FoxHound Implicit",
+                    ClientId = "foxhound2",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        //"https://localhost:44389/signout-callback-oidc"
+                    },
+                    RedirectUris = new List<string>( )
+                    {
+                        // "https://localhost:44389/signin-oidc"
                     },
                     AllowedScopes =
                     {
@@ -45,7 +101,6 @@ namespace FoxHound.IDP
                         new Secret("secret".Sha256())  // use different for production
                     }
                 }
-
             };
 
     }
