b80a6041fda66e0102528a5cceba64e10ee2543e
Jon Hart <62618553+jjh121@users.noreply.github.com>
2020-04-14 15:29:06 -0400

    Style all blogs view (#50)
    * Style all blogs view
    
    * Show number of posts

--- FoxHound.Client/src/Blogs/Blogs.tsx
+++ FoxHound.Client/src/Blogs/Blogs.tsx
@@ -1,12 +1,34 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
-import Blog from "./Blog";
 import axios from "axios";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  Avatar,
+  IconButton,
+  Container,
+  Box,
+  Paper,
+  Typography,
+  Divider,
+  Tooltip,
+  Badge,
+} from "@material-ui/core";
+import PersonIcon from "@material-ui/icons/Person";
+import EditIcon from "@material-ui/icons/Edit";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import DescriptionIcon from "@material-ui/icons/Description";
+import { Link } from "react-router-dom";
+import { PostModel } from "../Posts/Post";
 
 export interface BlogModel {
   blogId: number;
   owner: string;
   title: string;
   createdDate: Date;
+  posts: PostModel[];
 }
 
 const Blogs: React.FC = () => {
@@ -17,10 +39,6 @@ const Blogs: React.FC = () => {
   useEffect(() => {
     getBlogs();
   }, []);
-
-  const handleBlogAdded = (newBlogId: number) => {
-    getBlogs();
-  };
 
   const getBlogs = async () => {
     try {
@@ -39,23 +57,74 @@ const Blogs: React.FC = () => {
   };
 
   return (
-    <div>
-      <h3>Blogs</h3>
+    <Container maxWidth="lg">
+      <Paper elevation={5}>
+        <Box p={3}>
+          <Box display="flex">
+            <Box flexGrow={1}>
+              <Typography variant="h5">Blogs</Typography>
+            </Box>
+            <Box>
+              <Tooltip title="Add blog">
+                <IconButton component={Link} to="blog/addBlog">
+                  <AddCircleOutlineIcon></AddCircleOutlineIcon>
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Box>
 
-      {error && <div>{error}</div>}
+          {error && <div>{error}</div>}
 
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <ul>
-          {blogs.map((blog) => (
-            <li key={blog.blogId}>
-              <Blog blog={blog} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : (
+            <List dense>
+              {blogs.map((blog) => (
+                <React.Fragment key={blog.blogId}>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar component={Link} to={`/blog/${blog.blogId}`}>
+                        <PersonIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={blog.title}
+                      secondary={blog.owner}
+                    ></ListItemText>
+                    <ListItemSecondaryAction>
+                      <Tooltip title="Posts">
+                        <IconButton
+                          edge="end"
+                          component={Link}
+                          to={`/blog/${blog.blogId}`}
+                        >
+                          <Badge
+                            badgeContent={blog.posts.length}
+                            color="primary"
+                          >
+                            <DescriptionIcon />
+                          </Badge>
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Edit">
+                        <IconButton
+                          edge="end"
+                          component={Link}
+                          to={`/blog/edit/${blog.blogId}`}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                </React.Fragment>
+              ))}
+            </List>
+          )}
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
